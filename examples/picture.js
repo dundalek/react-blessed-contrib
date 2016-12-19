f726911365ff49485d4961302d8c1b3c7cb05de2
Jakub Dundalek <dundalek@gmail.com>
2016-12-19 16:07:32 +0100

    Add picture example

---
+++ examples/picture.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import blessed from 'blessed';
+import contrib from 'blessed-contrib';
+import { render } from 'react-blessed';
+import { Picture } from '../src/index';
+
+class App extends Component {
+  render() {
+    return (
+      <Picture file="./node_modules/png-js/images/laptop.png" cols={95} onReady={() => screen.render()} />
+    );
+  }
+}
+
+const screen = blessed.screen();
+render(<App />, screen);
